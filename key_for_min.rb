b33a7f0dcaa8b98fe42e94c77b1d7098376572c9
Kelin Field <kfield111@hotmail.com>
2020-08-08 03:42:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,8 @@
 
 def key_for_min_value(name_hash)
 name = nil
-new_array = []
 name_hash.each do |key, value|
 #store and compare data within the hash
-new_array.push value
-end
-new_array.each do |a, b|
- a <=> b
-name = new_array.first
 end
 name
 end
