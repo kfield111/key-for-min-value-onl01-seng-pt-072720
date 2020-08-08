12d3a020422317fd38975092052f3875a8545005
Kelin Field <kfield111@hotmail.com>
2020-08-08 03:04:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-captured_name = ""
+captured_name = 0
 name_hash.collect do |key, value|
-
+  if captured_name < value
+    captured_name = value
+  end
 end
 end
