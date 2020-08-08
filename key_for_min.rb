fdf232a7c91d4203ccfd60a4c274967817cc207a
Kelin Field <kfield111@hotmail.com>
2020-08-08 03:09:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,8 @@ captured_name = 0
 name_hash.each do |key, value|
   if value > captured_name
     captured_name = value
-    captured_name.first
+    captured_name
   end
+  captured_name.first.key
 end
 end
